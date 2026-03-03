e1de258a10ce89c34fd9250d4c4ee60dc6b2c420
Paul Hamilton <phamtec@mac.com>
2026-03-03 15:32:30 +1100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,15 @@ then
   exit 1
 fi
 
+uname -a | grep "6.17.0-1008-raspi"
+if [ "$?" == "0" ]; then
+  NPROC="-j2"
+else
+  NPROC="-j4"
+fi
+
+echo "Building with $NPROC"
+
 if [ ! -d "zmqscript/build" ];
 then
   mkdir zmqscript/build
@@ -18,7 +27,7 @@ cmake ..
 if [ "$1" == "newflo" ]; then
   rm -rf _deps/flo-*
 fi
-make
+make $NPROC
 [ "$?" != "0" ] && exit 1
 make test
 [ "$?" != "0" ] && exit 1
